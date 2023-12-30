ca36c3fe34583f1bc3cb7e872d394e9eb63cf5b9
Sonia <sonia09012004@gmail.com>
2023-12-30 15:36:24 +0200

    first

--- first part_rest api/src/database/db.py
+++ first part_rest api/src/database/db.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from src.conf.config import settings
+from src.conf import config
 
-SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
+SQLALCHEMY_DATABASE_URL = config.sqlalchemy_database_url
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
