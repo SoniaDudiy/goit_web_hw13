90a6846b56d7878f8b2c89cb9ddd7829788050cd
Sonia <sonia09012004@gmail.com>
2023-12-31 17:02:49 +0200

    first

--- first part_rest api/src/database/db.py
+++ first part_rest api/src/database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from src.conf import config
+from src.conf.config import config
 
 SQLALCHEMY_DATABASE_URL = config.sqlalchemy_database_url
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
